3353a697b218029fe1fb49987fbaabc802f6d99a
TsaiChihHan <tsaizhihan@gmail.com>
2017-10-17 23:50:40 -0600

    Graduation & Retention Fragment

--- app/src/main/java/studyelephant/com/studyelephant/fragments/GraduationFragment.java
+++ app/src/main/java/studyelephant/com/studyelephant/fragments/GraduationFragment.java
@@ -5,11 +5,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.google.gson.Gson;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import studyelephant.com.studyelephant.R;
 import studyelephant.com.studyelephant.model.College;
+
+import static android.graphics.Color.rgb;
 
 /**
  * Created by Zhihan on 10/17/2017.
@@ -18,9 +30,49 @@ import studyelephant.com.studyelephant.model.College;
 public class GraduationFragment extends Fragment {
 
     private College college;
+    private BarChart graduation_rate_chart;
+    private BarChart retention_rate_chart;
+    private TextView graduation;
+    private TextView retention;
 
     public GraduationFragment() {
         // Required empty public constructor
+    }
+
+    private void chartSetUp(BarChart chart, float collegeStat, float nationalAverage, float max, int color) {
+        List<BarEntry> entries = new ArrayList<BarEntry>();
+        entries.add(new BarEntry(0f, collegeStat));
+
+        BarDataSet set = new BarDataSet(entries, "");
+        set.setColor(color);
+        set.setDrawValues(false);
+        BarData data = new BarData(set);
+        data.setBarWidth(1f);
+
+        LimitLine ll = new LimitLine(nationalAverage, "");
+        ll.setLineWidth(2f);
+        ll.setLineColor(rgb(0, 0, 0));
+
+        chart.setData(data);
+        chart.setMinimumHeight(120);
+        chart.setExtraTopOffset(80f);
+        chart.setExtraBottomOffset(10f);
+        chart.setDrawBorders(true);
+        chart.setTouchEnabled(false);
+        chart.getDescription().setEnabled(false);
+        chart.getLegend().setEnabled(false);
+        chart.getAxisLeft().setDrawGridLines(false);
+        chart.getAxisLeft().setDrawLabels(false);
+        chart.getXAxis().setDrawGridLines(false);
+        chart.getXAxis().setEnabled(false);
+        chart.getAxisRight().setDrawGridLines(false);
+        chart.getAxisRight().setEnabled(false);
+        chart.getAxisLeft().setDrawAxisLine(false);
+        chart.getAxisLeft().setAxisMinimum(0);
+        chart.getAxisLeft().setAxisMaximum(max);
+        chart.getAxisLeft().setDrawTopYLabelEntry(false);
+        chart.getAxisLeft().addLimitLine(ll);
+        chart.invalidate();
     }
 
     @Override
@@ -37,6 +89,18 @@ public class GraduationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_graduation, container, false);
+        View view = inflater.inflate(R.layout.fragment_graduation, container, false);
+
+        this.graduation_rate_chart = (BarChart) view.findViewById(R.id.graduation_rate);
+        this.retention_rate_chart = (BarChart) view.findViewById(R.id.retention_rate);
+
+        chartSetUp(graduation_rate_chart, (float) college.completion_rate, 0.42f, 1f, rgb(57,132,182));
+        chartSetUp(retention_rate_chart, 0.9f, 0.68f, 1f, rgb(57,132,182));
+
+        this.graduation = (TextView) view.findViewById(R.id.graduation);
+        this.graduation.setText(NumberFormat.getPercentInstance().format(college.completion_rate));
+        this.retention = (TextView) view.findViewById(R.id.retention);
+        this.retention.setText(NumberFormat.getPercentInstance().format(0.9f));
+        return view;
     }
 }
