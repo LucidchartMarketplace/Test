3d22ebd3ee46495324ede8f2f1f9921bd65c05ee
gn02519081 <liao4@byu.edu>
2017-10-24 10:25:34 -0600

    "Enable back button"

--- app/src/main/java/studyelephant/com/studyelephant/CollegeDetailActivity.java
+++ app/src/main/java/studyelephant/com/studyelephant/CollegeDetailActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.google.gson.Gson;
 
@@ -35,6 +36,14 @@ public class CollegeDetailActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
